dd635d7e39ed465d892a23312ac25c577168f42f
Ellis Collinson <elliscollinson@me.com>
2024-04-15 19:18:57 +0100

    Update styles on the bottom player.

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,9 @@ import {
   VscChevronLeft,
   VscChevronRight,
   VscSearch,
-  VscPlay,
 } from "react-icons/vsc";
+
+import { IoPlayCircle, IoPauseCircle } from "react-icons/io5";
 
 import MiniCard from "./components/mini-cards/MiniCard";
 import MadeForYou from "./components/made-for-you/MadeForYou";
@@ -46,7 +47,7 @@ export default function Home() {
       </div>
 
       <div className="me-3 mt-2 w-8/12 xl:w-full">
-        <section className="h-full rounded-xl bg-gradient-to-b from-[#242424] to-[#000] px-4 pt-3">
+        <section className="h-full rounded-xl bg-gradient-to-b from-[#0b1b36] to-[#000] px-4 pt-3">
           <div className="flex w-full items-center justify-between">
             <div className="flex">
               <div className="flex items-center gap-2 pe-5">
@@ -57,11 +58,6 @@ export default function Home() {
                   <VscChevronRight size={25} />
                 </div>
               </div>
-              {/* <input
-                type="text"
-                placeholder="What do you want to listen to?"
-                className="h-full w-80 rounded-full bg-neutral-800 p-5"
-              /> */}
             </div>
 
             <div className="flex h-10 gap-3">
@@ -83,31 +79,33 @@ export default function Home() {
         </section>
       </div>
       <div className="fixed bottom-0 flex h-20 w-full bg-black">
-        <div className="w-4/12 ">1</div>
+        <div className="w-4/12"></div>
         <div className="mb-2 mt-2 flex w-5/12 flex-col ">
           <div className="flex h-2/3 justify-center ">
-            <div className="flex w-64 justify-center ">
-              <div className="flex w-14 items-center justify-center">
+            <div className="flex w-64 justify-center text-neutral-600">
+              <div className="flex w-10 items-center justify-center">
                 <FaShuffle />
               </div>
-              <div className="flex w-14 items-center justify-center">
+              <div className="flex w-10 items-center justify-center">
                 <FaFastBackward />
               </div>
-              <div className="flex w-14 items-center justify-center  ">
-                <div className="flex h-8 w-8 items-center justify-center rounded-full bg-white text-xl ">
-                  <VscPlay />
+              <div className="flex w-10 items-center justify-center">
+                <div className="flex w-10 items-center justify-center rounded-full text-7xl ">
+                  <IoPauseCircle />
                 </div>
               </div>
-              <div className="flex  w-14 items-center justify-center">
+              <div className="flex w-10 items-center justify-center">
                 <FaFastForward />
               </div>
-              <div className="flex  w-14 items-center justify-center">
+              <div className="flex w-10 items-center justify-center">
                 <FaRepeat />
               </div>
             </div>
           </div>
-          <div className="flex  h-1/3 items-center">
+          <div className="flex  h-1/3 items-center text-neutral-400">
+            <div className="mx-1 flex w-10 justify-center text-xs">-:--</div>
             <div className="h-1 w-full rounded bg-neutral-600"></div>
+            <div className="mx-1 flex w-10 justify-center text-xs">-:--</div>
           </div>
         </div>
         <div className="w-4/12 ">3</div>
