d553044f99759238f63b7b425f69678fa88e295b
Ellis Collinson <elliscollinson@me.com>
2024-06-18 17:41:44 +0100

    ✨ Add 'download', profile, and notification buttons.x

--- app/page.tsx
+++ app/page.tsx
@@ -14,13 +14,20 @@ import MadeForYou from "./components/made-for-you/MadeForYou";
 import YourLibrary from "./components/your-library/YourLibrary";
 import { FaFastBackward, FaFastForward } from "react-icons/fa";
 import { FaRepeat, FaShuffle } from "react-icons/fa6";
-import { GoHomeFill, GoSearch } from "react-icons/go";
+import {
+  GoBell,
+  GoDownload,
+  GoHomeFill,
+  GoPerson,
+  GoSearch,
+} from "react-icons/go";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-row">
+      {/* --------------------------------- sidebar -------------------------------- */}
       <div className="m-2 flex w-4/12 min-w-[300px] flex-col gap-2 xl:w-[500px]">
-        <section className="bg-sidebar flex flex-col gap-6 p-6">
+        <section className="flex flex-col gap-6 bg-sidebar p-6">
           <span className="flex items-center gap-5 text-white">
             <GoHomeFill size={30} />
             <h3 className="font-semibold">Home</h3>
@@ -31,26 +38,26 @@ export default function Home() {
           </span>
         </section>
 
-        <section className="bg-sidebar h-full rounded-xl p-6">
+        <section className="h-full rounded-xl bg-sidebar p-6">
           <div className="flex w-full justify-between">
             <h3 className="font-semibold text-neutral-400">Your Library</h3>
-            <span className="flex w-2/12 items-center justify-between text-neutral-400">
+            <span className="flex w-[50px] items-center justify-between text-neutral-400">
               <VscAdd
-                size={24}
+                size={17}
                 className="transition duration-300 ease-in-out hover:text-neutral-100"
               />
               <VscArrowRight
-                size={24}
+                size={17}
                 className="transition duration-300 ease-in-out hover:text-neutral-100"
               />
             </span>
           </div>
           <div className="flex flex-col">
-            <div className="mt-4 flex gap-2">
-              <button className="bg-side_badge rounded-full px-4 py-2 ">
+            <div className="mt-4 flex gap-2 text-xs">
+              <button className="rounded-full bg-side_badge px-4 py-2 ">
                 Playlists
               </button>
-              <button className="bg-side_badge rounded-full px-4 py-2">
+              <button className="rounded-full bg-side_badge px-4 py-2">
                 Albums
               </button>
             </div>
@@ -59,6 +66,7 @@ export default function Home() {
         </section>
       </div>
 
+      {/* --------------------------------- main content -------------------------------- */}
       <div className="me-3 mt-2 w-8/12 xl:w-full">
         <section className="h-full rounded-xl bg-gradient-to-b from-[#0b1b36] to-[#000] px-4 pt-3">
           <div className="flex w-full items-center justify-between">
@@ -77,17 +85,23 @@ export default function Home() {
               <button className="h-8 rounded-full bg-white px-4 text-sm font-semibold text-black">
                 Explore Premium
               </button>
-              <button className="h-8 rounded-full bg-black px-4 text-sm font-semibold text-white">
+              <button className="flex h-8 items-center justify-center gap-2 rounded-full bg-black px-4 text-sm font-semibold text-white">
+                <GoDownload size={17} />
                 Install app
               </button>
-              <span>notifications profile</span>
+              <button className="flex h-8 w-8 items-center justify-center rounded-full bg-black text-sm font-semibold text-white">
+                <GoBell size={17} />
+              </button>
+              <button className="flex h-8 w-8 items-center justify-center rounded-full bg-black text-sm font-semibold text-white">
+                <GoPerson size={17} />
+              </button>
             </div>
           </div>
-          <h3 className="mb-4 mt-8">Good morning</h3>
+          <h2 className="mb-4 mt-8">Good morning</h2>
           <MiniCard />
-          <h3 className="mb-4 mt-8">Made for you</h3>
+          <h2 className="mb-4 mt-8">Made for you</h2>
           <MadeForYou />
-          <h3 className="mb-4 mt-8">Popular Radio</h3>
+          <h2 className="mb-4 mt-8">Popular Radio</h2>
           <MadeForYou />
         </section>
       </div>
